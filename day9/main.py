19c18c97377340ca28800f11aefe95c37abd4390
Erlend Paulsen Skaaden <erlendskaaden@gmail.com>
2022-12-09 12:36:25 +0100

    finish day 9

--- day9/main.py
+++ day9/main.py
@@ -5,45 +5,41 @@ head = [0, 0]
 tails = [[0, 0] for _ in range(9)]
 tail_visited = set([])
 
-# order is basically if we are moving along a column or row
-# order = (0, 1) = column
-# order = (1, 0) = row
-
 
 def is_neighbor(head, tail):
     return abs(head[0] - tail[0]) <= 1 and abs(head[1] - tail[1]) <= 1
 
 
-def update_tail(order: tuple, sign: int):
+def update_tail():
     curr_head = head
     for i in range(len(tails)):
         curr_tail = tails[i]
-        if is_neighbor(curr_head, curr_tail):
-            pass
-        elif curr_head[order[0]] == curr_tail[order[0]]:
-            curr_tail[order[1]] += sign
-        elif curr_head[order[0]] != curr_tail[order[0]]:
-            if curr_head[order[0]] - curr_tail[order[0]] < 0:
-                to_add = max(curr_head[order[0]] - curr_tail[order[0]], -1)
-            else:
-                to_add = min(curr_head[order[0]] - curr_tail[order[0]], 1)
-            curr_tail[order[0]] += to_add
-            curr_tail[order[1]] += sign
+        if not is_neighbor(curr_head, curr_tail):
+            if (diff_col := curr_head[0] - curr_tail[0]) != 0:
+                if diff_col < 0:
+                    curr_tail[0] -= 1
+                else:
+                    curr_tail[0] += 1
+            if (diff_row := curr_head[1] - curr_tail[1]) != 0:
+                if diff_row < 0:
+                    curr_tail[1] -= 1
+                else:
+                    curr_tail[1] += 1
+
         curr_head = tails[i]
 
 
-with open("test2.txt", "r") as f:
+with open("input.txt", "r") as f:
     for line in f.readlines():
         line = line.strip().split(" ")
         sign = sign_map[line[0]]
+        orientation = 0  # move along row
         if line[0] == "U" or line[0] == "D":
-            order = (0, 1)  # move along column
-        else:
-            order = (1, 0)  # move along row
+            orientation = 1  # move along column
 
         for _ in range(int(line[1])):
-            head[order[1]] += sign
-            update_tail(order, sign)
+            head[orientation] += sign
+            update_tail()
             tail_visited.add(tuple(tails[-1]))
 
 
